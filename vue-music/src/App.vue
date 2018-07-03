4651a5546aad54dc2028f48e588104a9ed7d804a
郑顺东哒哒哒 <33625419+zhengshundong01@users.noreply.github.com>
2018-07-03 23:36:23 +0800

    zsd
    new pro

---
+++ vue-music/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+/* eslint-disable indent */
+
+  import MHeader from 'components/m-header/m-header'
+
+  export default {
+    components: {
+      MHeader: MHeader
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
