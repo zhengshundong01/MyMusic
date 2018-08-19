52bd078619ffe862a5994eb90a37a909e7e70471
郑顺东哒哒哒 <33625419+zhengshundong01@users.noreply.github.com>
2018-08-19 22:42:58 +0800

    aa

---
+++ vue-music/src/api/recommend.js
@@ -0,0 +1,14 @@
+import jsnop from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getRecommend () {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+
+  return jsnop(url, data, options)
+}
