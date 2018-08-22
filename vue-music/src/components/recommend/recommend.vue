f8b37b890d9a8e9015e9ed36489bca3c7d8a40e1
郑顺东哒哒哒 <33625419+zhengshundong01@users.noreply.github.com>
2018-08-22 23:04:46 +0800

    更新

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
+      <div v-if="recommends.length" class="slider-wrapper">
         <slider>
           <div v-for="(item,index) in recommends" :key="index">
             <a :href="item.linkUrl">
@@ -13,6 +13,15 @@
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul>
+          <li class="item" v-for="(item, index) in discList" :key="index">
+            <div class="icon">
+              <img :src="item.imgurl" width="60" height="60">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
@@ -22,7 +31,7 @@
 <script type="text/ecmascript-6">
 /* eslint-disable indent */
 
-  import { getRecommend } from 'api/recommend'
+  import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
   import Slider from 'base/slider/slider.vue'
 
@@ -30,17 +39,26 @@
     components: {Slider},
     data () {
       return {
-        recommends: {}
+        recommends: {},
+        discList: []
       }
     },
     created () {
       this._getRecommond()
+      this._getDiscList()
     },
     methods: {
       _getRecommond () {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList () {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
           }
         })
       }
